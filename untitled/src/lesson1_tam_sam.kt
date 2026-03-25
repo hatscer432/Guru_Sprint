7ffdbc593f8a17765277b6a0d585e96fec0f8569
muham <muhammat.2907@gmail.com>
2026-03-25 11:13:46 +0300

    gs -1 -1 -4

--- untitled/src/lesson1_tam_sam.kt
+++ untitled/src/lesson1_tam_sam.kt
@@ -1,4 +1,52 @@
-fun  main(args:Array<String>){
-    "ехал грека через реку види грека в реке рак сунул грека руку в реку вдруг рак за руку цап"
-}
+import java.util.*
 
+fun main() {
+    val scanner = Scanner(System.`in`)
+    val queue: Queue<String> = LinkedList()
+    var totalSales = 0.0
+
+    while (true) {
+        println("\n=== КАССОВОЕ ОБСЛУЖИВАНИЕ ===")
+        println("1. Добавить клиента в очередь")
+        println("2. Обслужить клиента")
+        println("3. Показать очередь")
+        println("4. Показать общую выручку")
+        println("5. Выход")
+        print("Выберите действие: ")
+
+        when (scanner.nextInt()) {
+            1 -> {
+                print("Введите имя клиента: ")
+                val name = scanner.next()
+                queue.add(name)
+                println("Клиент $name добавлен в очередь")
+            }
+            2 -> {
+                if (queue.isNotEmpty()) {
+                    val client = queue.poll()
+                    print("Введите сумму покупки для $client: ")
+                    val amount = scanner.nextDouble()
+                    totalSales += amount
+                    println("Клиент $client обслужен на сумму ${String.format("%.2f", amount)} руб.")
+                } else {
+                    println("Очередь пуста, некого обслуживать")
+                }
+            }
+            3 -> {
+                if (queue.isNotEmpty()) {
+                    println("Очередь клиентов: $queue")
+                } else {
+                    println("Очередь пуста")
+                }
+            }
+            4 -> {
+                println("Общая выручка: ${String.format("%.2f", totalSales)} руб.")
+            }
+            5 -> {
+                println("До свидания!")
+                return
+            }
+            else -> println("Неверный выбор, попробуйте снова")
+        }
+    }
+}
